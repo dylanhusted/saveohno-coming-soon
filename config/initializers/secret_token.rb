b7979d1a55f8e6de8d2db6f1ad7b0f022b013bba
Dylan Husted <dhusted1@babson.edu>
2014-09-20 12:06:49 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Saveohno::Application.config.secret_key_base = '5edd9ad7e0038ad881ff5d8ecf9044a49b3ab6334169a0f0a8e072a1f3aef807d8e3af014ec8dce05d1807d779e8fafeefd03ff166c59cbf5514b1a16584d283'
