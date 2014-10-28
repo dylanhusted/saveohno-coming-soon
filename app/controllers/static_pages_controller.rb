ce23cb02dce00b61be77ffcf8f8b255dbec79bc4
Dylan Husted <dhusted1@babson.edu>
2014-10-27 20:57:58 -0400

    Switched to static bg images. Now allowing iframe embeds.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,16 @@
 class StaticPagesController < ApplicationController
-	def home
-		@pre_launch_user = PreLaunchUser.new
-	end
+	  after_action :allow_iframe, only: :embed
+
+  def embed
+  end
+
+  def home
+	@pre_launch_user = PreLaunchUser.new
+  end
+
+private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
 end
