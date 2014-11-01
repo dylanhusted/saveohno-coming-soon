1f7a2678fb0f768084229ee1a2a1f6ebc911ae1c
Dylan Husted <dhusted1@babson.edu>
2014-11-01 10:58:01 -0400

    Changed font declerations, edited start here positioning and reduced font size of whoisohno

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
         $('header').css("height", "12%");
         $('.logo').css("width", "12%");
         $('.points').css("font-size", "55%");
-        $('.ohno-sad').css("width", "4%");
         $('.ohno-neutral').css("width", "5%");
         $('.ohno-tornado').css("width", "10%");
-        $('.who-is-ohno-content').css("font-size", "1.5em");
+        $('.who-is-ohno-content').css("font-size", "1.45em");
         $('.what-is-saveohno-content').css("font-size", "1.5em");
         $('.stay-updated-content').css("font-size", "1.5em");
+        $('#start-here').css("margin-top", "5%");
+        $('#start-here').css("margin-right", "2%");
         $('#start-here').css("width", "40%");
     }
 
