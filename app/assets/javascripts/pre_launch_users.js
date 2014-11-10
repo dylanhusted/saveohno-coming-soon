c6374ab7556112b669b5ed1edc32eb35cf7b3bb4
Dylan Husted <dhusted1@babson.edu>
2014-11-10 14:58:48 -0500

    Mobile specific styling

--- app/assets/javascripts/pre_launch_users.js
+++ app/assets/javascripts/pre_launch_users.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         $('header').addClass('importantHeight');
         $('.perk').css('width', '82.75%');
+        $('.perk-small').css('height', '450px');
+        $('.perk-med').css('height', '535px');
+        $('.perk-big').css('height', '575px');
+        $('.perk-huge').css('height', '610px');
+        $('.perk-img').css('width', '10%');
         $('.spacing').html();
-        $('.perk').css('font-size', '1.25em');
+        $('.perk').css('font-size', '1.75em');
         $('.not-perk').css('font-size', '1.25em');
     }
 
